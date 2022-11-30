c7e329bd712e858fbeda2b015a59bb1b7df43e0d
Santhosh kumar <84260938+SanthoshKumarrS@users.noreply.github.com>
2022-12-01 04:28:25 +0530

    Add files via upload

---
+++ 05_Deployment/serving churn Model.py
@@ -0,0 +1,28 @@
+import ssl
+
+import requests
+
+url = 'https://localhost:9696/predict'
+
+customer = {
+    "gender": "female",
+    "seniorcitizen": 0,
+    "partner": "yes",
+    "dependents": "no",
+    "phoneservice": "no",
+    "multiplelines": "no_phone_service",
+    "internetservice": "dsl",
+    "onlinesecurity": "no",
+    "deviceprotection": "no",
+    "techsupport": "no",
+    "streamingtv": "no",
+    "streamingmovies": "no",
+    "contract": "month-to-month",
+    "paperlessbilling": "yes",
+    "paymentmethod": "electronic_check",
+    "tenure": 1,
+    "monthlycharges": 29.85,
+    "totalcharges": 29.85
+}
+
+requests.post(url, verify=ssl.CERT_NONE)
